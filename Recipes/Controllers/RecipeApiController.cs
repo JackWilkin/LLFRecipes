86d096959c726e5a6b2a00b92b31b04abae35728
Jack Wilkin <wilkin.j@husky.neu.edu>
2018-04-08 17:23:02 -0400

    Fix Tech Debt

--- Recipes/Controllers/RecipeApiController.cs
+++ Recipes/Controllers/RecipeApiController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -11,10 +12,38 @@ namespace Recipes.Controllers
     [System.Web.Http.RoutePrefix("api/recipe")]
     public class RecipeApiController : ApiController
     {
-
-        public Recipe GetRecipe()
+        public static Recipe AppleCrisp()
         {
-            return Recipe.AppleCrisp();
+            Ingredient apple = new Ingredient(0, "Whole Apples", 1.5, Unit.Self);
+            Ingredient sugar = new Ingredient(1, "Sugar", 1, Unit.Cup);
+            Ingredient cinnamon = new Ingredient(2, "Cinnamon", 2, Unit.Tbs);
+            Ingredient salt = new Ingredient(3, "Salt", .5, Unit.tsp);
+            List<Ingredient> ingredients = new List<Ingredient>(new Ingredient[] { apple, sugar, cinnamon, salt });
+
+            Utensil whisk = new Utensil(0, "Whisk", "Whisk it");
+            Utensil bowl = new Utensil(1, "Bowl", "Stir in it");
+            List<Utensil> utensils = new List<Utensil>(new Utensil[] { whisk, bowl });
+
+            Recipe appleCrisp = new Recipe(0, "Apple Crisp", ingredients, utensils, 350, "Whisk it all in a bowl", false);
+
+            return appleCrisp;
+        }
+
+        public static Recipe ChocolateCake()
+        {
+            Ingredient butter = new Ingredient(0, "Butter", 1.5, Unit.Cup);
+            Ingredient sugar = new Ingredient(1, "Sugar", 1, Unit.Cup);
+            Ingredient chocolate = new Ingredient(2, "Chocolate", 2, Unit.Tbs);
+            Ingredient salt = new Ingredient(3, "Salt", .5, Unit.tsp);
+            List<Ingredient> ingredients = new List<Ingredient>(new Ingredient[] { butter, sugar, chocolate, salt });
+
+            Utensil whisk = new Utensil(0, "Whisk", "Whisk it");
+            Utensil bowl = new Utensil(1, "Bowl", "Stir in it");
+            List<Utensil> utensils = new List<Utensil>(new Utensil[] { whisk, bowl });
+
+            Recipe chocolateCake = new Recipe(0, "Chocolate Cake", ingredients, utensils, 350, "Whisk it all in a bowl", false);
+
+            return chocolateCake;
         }
 
         [System.Web.Http.HttpGet]
@@ -27,7 +56,7 @@ namespace Recipes.Controllers
             {             
                 var result = await Task.Run(() => {
                     //Get the order details first: 
-                    var recipeDetail = Recipe.AppleCrisp();
+                    var recipeDetail = AppleCrisp();
                     return recipeDetail;
                 });
 
--- Recipes/Models/Recipe.cs
+++ Recipes/Models/Recipe.cs
@@ -143,38 +143,6 @@ namespace Recipes.Models
             return new JavaScriptSerializer().Serialize(this);
         }
 
-        //////////////////////////Testing/////////////////////////
-        public static Recipe AppleCrisp() {
-            Ingredient apple = new Ingredient(0, "Whole Apples", 1.5, Unit.Self);
-            Ingredient sugar = new Ingredient(1, "Sugar", 1, Unit.Cup);
-            Ingredient cinnamon = new Ingredient(2, "Cinnamon", 2, Unit.Tbs);
-            Ingredient salt = new Ingredient(3, "Salt", .5, Unit.tsp);
-            List<Ingredient> ingredients = new List<Ingredient>(new Ingredient[] { apple, sugar, cinnamon, salt });
-
-            Utensil whisk = new Utensil(0, "Whisk", "Whisk it");
-            Utensil bowl = new Utensil(1, "Bowl", "Stir in it");
-            List<Utensil> utensils = new List<Utensil>(new Utensil[] { whisk, bowl });
-
-            Recipe appleCrisp = new Recipe(0, "Apple Crisp", ingredients, utensils, 350, "Whisk it all in a bowl", false);
-
-            return appleCrisp;
-        }
-
-        public static Recipe ChocolateCake()
-        {
-            Ingredient butter = new Ingredient(0, "Butter", 1.5, Unit.Cup);
-            Ingredient sugar = new Ingredient(1, "Sugar", 1, Unit.Cup);
-            Ingredient chocolate = new Ingredient(2, "Chocolate", 2, Unit.Tbs);
-            Ingredient salt = new Ingredient(3, "Salt", .5, Unit.tsp);
-            List<Ingredient> ingredients = new List<Ingredient>(new Ingredient[] { butter, sugar, chocolate, salt });
-
-            Utensil whisk = new Utensil(0, "Whisk", "Whisk it");
-            Utensil bowl = new Utensil(1, "Bowl", "Stir in it");
-            List<Utensil> utensils = new List<Utensil>(new Utensil[] { whisk, bowl });
-
-            Recipe chocolateCake = new Recipe(0, "Chocolate Cake", ingredients, utensils, 350, "Whisk it all in a bowl", false);
-
-            return chocolateCake;
-        }
+      
     }
 }
